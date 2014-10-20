4c9701d1e19d6613eb2eb851237ad8a2e14c16bb
Matt Halverson <mhalverson@onekingslane.com>
2014-10-20 15:17:15 -0700

    Remove commented code

--- cfg/.lein/profiles.clj
+++ cfg/.lein/profiles.clj
@@ -2,7 +2,5 @@
                   [lein-marginalia "0.7.1"]
                   [lein-pprint "1.1.1"]
                   [lein-localrepo "0.5.2"]
-                  ;; use nREPL now
-                  ;; [lein-swank "1.4.4"]
                   [lein-clojars "0.9.0"]
                   [lein-kibit "0.0.8"]]}}
